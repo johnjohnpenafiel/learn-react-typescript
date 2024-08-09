32ee74f15e7271516b9496403bdbeb7703cd3e24
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-08-09 16:42:31 -0400

    styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
 
+  const items = ["New York", "Los Angeles", "Miami"];
+
+  const onSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      {alertVisible && (
-        <Alert
-          onClick={() => {
-            setAlertVisible(false);
-          }}
-        >
-          Alert
-        </Alert>
-      )}
-      <Button onClick={() => setAlertVisible(true)}>Alert</Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItem={onSelectedItem}
+      />
     </div>
   );
 }
