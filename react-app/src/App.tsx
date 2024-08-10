f996d1f28179fc0088959400587b92b706e146a9
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-08-10 15:11:14 -0400

    updating nested arrays and objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,23 +5,36 @@ import Button from "./components/Button";
 import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
-  const items = ["New York", "Los Angeles", "Miami"];
-
-  const onSelectedItem = (item: string) => {
-    console.log(item);
+  const handleClick = () => {
+    const addProduct = {
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    };
+    setCart(addProduct);
   };
 
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectedItem={onSelectedItem}
-      />
-    </div>
+    <>
+      <h1>Items</h1>
+      <ul>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            Name:{item.title} Quanity:{item.quantity}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Add Product</button>
+    </>
   );
 }
-
 export default App;
