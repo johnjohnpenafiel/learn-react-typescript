7c1ee183a42842f92afd0a6ea6ac3950c51cd0a7
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-08-22 12:21:26 -0400

    react-hook-form library

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,23 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText>
-        In a quiet village nestled between towering mountains, lived a young
-        girl named Elara who dreamed of touching the sky. Every evening, she
-        would climb the tallest hill and reach out, her fingers grazing the
-        clouds in her imagination. One day, a great storm rolled in, and the
-        villagers sought shelter. But Elara stood firm on her hill, her heart
-        filled with determination. As the wind howled, a brilliant flash of
-        lightning illuminated the sky, revealing a hidden path made of glowing
-        stones leading upwards. Without hesitation, Elara followed it, climbing
-        higher and higher until she found herself standing among the stars. The
-        storm subsided, and Elara returned, her hands filled with stardust. From
-        that day on, the village prospered, for Elara had brought back a piece
-        of the sky, reminding everyone that dreams, no matter how lofty, could
-        become reality.
-      </ExpandableText>
+      <Form />
     </>
   );
 }
---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,42 @@
+import { FieldValues, useForm } from "react-hook-form";
+
+function Form() {
+  // You can check the attributes by logging the object
+  const form = useForm();
+  console.log(form);
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
