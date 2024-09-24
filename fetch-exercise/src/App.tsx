dd6d040bace8d1e9cedec003ad0da1daa619b2bd
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-09-24 17:42:42 -0400

    fetch typescript + loaders

--- fetch-exercise/src/App.tsx
+++ fetch-exercise/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 import "./App.css";
 
@@ -10,15 +10,32 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [errors, setErrors] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErrors(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return (
     <>
+      {errors && <p>{errors}</p>}
+      {isLoading && "loading..."}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
